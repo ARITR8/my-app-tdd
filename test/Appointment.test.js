b9efe3f99a49824590e11361c8b1ca14966f7c6e
Ari <aritradas75@gmail.com>
2024-12-11 02:23:07 +0530

    Before each change along with Refractoring using Props

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -3,6 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Appointment } from "../src/Appointment";
 import { act } from "react-dom/test-utils";
+
+let container;
+
+beforeEach(() => {
+     container = document.createElement('div');
+});
+
 describe("Test Suite Name", () => {
 
     //ReactDOM.createRoot(container).render(component);
@@ -19,7 +26,29 @@ describe("Appointment", () => {
         const component = <Appointment customer={customer} />; // Create component instance
        // const container = document.createElement('div');
         // Create a container for rendering
-        const container = document.createElement('div');
+       // const container = document.createElement('div');
+
+        // Render the component into the container using createRoot
+        // Use act to handle the async rendering
+        await act(async () => {
+            ReactDOM.createRoot(container).render(component);
+        });
+        expect(container.textContent).toContain("Ashley");
+
+    });
+
+});
+
+
+    //triangulate example
+    describe("Appointment", () => {
+
+    it("renders the customer first name", async ()=> {
+        const customer = { firstName: "Ashley" }; // Customer data
+        const component = <Appointment customer={customer} />; // Create component instance
+       // const container = document.createElement('div');
+        // Create a container for rendering
+     //   const container = document.createElement('div');
 
         // Render the component into the container using createRoot
         // Use act to handle the async rendering
