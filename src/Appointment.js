e7f851bf1068ad88abe6eb4965fb3cbeb46c1da9
Ari <aritradas75@gmail.com>
2024-12-12 02:57:34 +0530

    Passed the test after adding the ID of div

--- src/Appointment.js
+++ src/Appointment.js
@@ -5,4 +5,8 @@ export const Appointment = ({ customer }) => {
     return <div>{ customer.firstName }</div>; // Wrap the string in a <div> or any other element
 };
 
-export const AppointmentDayView = () => { };
+export const AppointmentDayView = () => {
+
+   return <div id="appointmentsDayView"></div>
+
+};
